a9736c3635fc86cfc0072ef1481b18bc9007d732
PhamQuocCong1995 <quoccong1781995@gmail.com>
2023-08-30 13:39:14 +0700

    work responsive

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,15 @@
 const faqs = document.querySelectorAll(".faq-item");
+const dots = document.querySelectorAll(".dot");
 
 faqs.forEach((faq) => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("active");
+    });
+});
+
+dots.forEach((dot) => {
+    dot.addEventListener("click", () => {
+        document.querySelector(".active")?.classList.remove("active");
+        dot.classList.add("active");
     });
 });
